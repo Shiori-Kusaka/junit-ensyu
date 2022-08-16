f893a914b0521d15b1c7e846b2bda989b8ac779e
Shiori-Kusaka <shiori.kusaka@rakus-partners.co.jp>
2022-08-16 14:08:06 +0900

    NetworkResourcesTest作成

---
+++ src/test/java/junit/tutorial/ex03/e03/NetworkResourcesTest.java
@@ -0,0 +1,68 @@
+package junit.tutorial.ex03.e03;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+class NetworkResourcesTest {
+	@InjectMocks  
+	private NetworkResources networkResources;
+	private String StInput = "Hello World";
+	byte[] bytes = StInput.getBytes(StandardCharsets.UTF_8);
+	InputStream input = new ByteArrayInputStream(bytes);
+	
+	
+	
+	@Mock
+	//private NetworkLoader mockLoader;
+	private NetworkLoader mockLoader;
+	
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() throws IOException {
+		//NetworkResources networkResources = new NetworkResources();
+		doReturn(input).when(mockLoader).getInput();
+		//doReturn(new String(StInput)).when(mockLoader).getInput();
+		String result = networkResources.load();
+		assertEquals(StInput, result);
+		
+		
+		
+		
+	}
+
+}
