1bdf749df363f0eaddf63966ff6ef016a7f8f147
Shiori-Kusaka <shiori.kusaka@rakus-partners.co.jp>
2022-08-16 09:30:52 +0900

    MonthlyCalenderTest作成

---
+++ src/test/java/junit/tutorial/ex03/e01/MonthlyCalendarTest.java
@@ -0,0 +1,37 @@
+package junit.tutorial.ex03.e01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MonthlyCalendarTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		MonthlyCalendar monthlyCalendar = new MonthlyCalendar();
+		int result = monthlyCalendar.getRemainingDays();
+		System.out.println(result);
+		assertEquals(15, result);
+	}
+
+}
